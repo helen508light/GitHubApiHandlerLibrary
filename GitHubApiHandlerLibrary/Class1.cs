6d33ca0f7cc08672b836fb34d71320f9ee60a95a
helen508light <helen508light@gmail.com>
2018-05-14 16:48:52 +0300

    initial commit

---
+++ GitHubApiHandlerLibrary/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace GitHubApiHandlerLibrary
+{
+    public class GithubApiHandler
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private const string GithubApiDotnetEventsUrl = "https://api.github.com/orgs/dotnet/events",
+            GithubApiDotnetReposUrl = "https://api.github.com/orgs/dotnet/repos",
+            GithubApiUsersUrl = "https://api.github.com/users/";
+
+        private static void PrepareClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+        }
+
+        public static async Task<List<Repository>> ProcessRepositories(string username = "", int page = 1)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+
+            try
+            {
+                PrepareClient();
+                string url = string.IsNullOrEmpty(username) ? GithubApiDotnetReposUrl : string.Format("{0}{1}/repos", GithubApiUsersUrl, username);
+                url += string.Format("?page={0}", page);
+                var streamTask = await client.GetStreamAsync(url);
+                var repositories = serializer.ReadObject(streamTask) as List<Repository>;
+
+                return repositories;
+            }
+            catch (AggregateException ae)
+            {
+                Console.WriteLine("One or more exceptions occurred: ");
+                foreach (var ex in ae.Flatten().InnerExceptions)
+                    Console.WriteLine("   {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                if (ex is HttpRequestException || ex is TimeoutException || ex is TaskCanceledException)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", ex.Message);
+                }
+            }
+
+            return null;
+        }
+
+
+        public static IEnumerable<Repository> GetRepositoryByLinqCriteria(List<Repository> repositories)
+        {
+            IEnumerable<Repository> res = from r in repositories where r.forksCount > 100 && !r.isPrivate && r.Watchers > 100 select r;
+
+            return res;
+        }
+
+        public static Repository GetRepoWithMaxWatchersCount(List<Repository> repositories)
+        {
+            return repositories.Aggregate((r1, r2) => r1.Watchers > r2.Watchers ? r1 : r2);
+        }
+
+        public static double GetAverageWatchersCount(List<Repository> repositories)
+        {
+            return repositories.Average(repo => repo.Watchers);
+        }
+
+        public static Task<List<Repository>> GetReposByUserName(string username, int page = 1)
+        {
+            return ProcessRepositories(username, page);
+        }
+    }
+}
---
+++ GitHubApiHandlerLibrary/repo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Globalization;
+
+namespace GitHubApiHandlerLibrary
+{
+    [DataContract(Name = "repo")]
+    public class Repository
+    {
+        private string _name;
+
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get { return this._name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("String is null or empty");
+                this._name = value;
+            }
+        }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "pushed_at")]
+        private string JsonDate { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
+
+        [DataMember(Name = "private")]
+        public bool isPrivate { get; set; }
+
+        [DataMember(Name = "forks")]
+        public int forksCount { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+    }
+}
